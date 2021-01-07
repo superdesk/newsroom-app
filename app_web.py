7f487b2362d9f7e8bc2c53e50bf610035fcd257d
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-07 17:21:15 +0100

    Add web_app.py and requirements.txt

---
+++ app_web.py
@@ -0,0 +1,12 @@
+import os
+
+from newsroom.web import NewsroomWebApp
+
+
+
+app = NewsroomWebApp(__name__)
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = int(os.environ.get('PORT', '5050'))
+    app.run(debug=True, host=host, port=port, threaded=True)
