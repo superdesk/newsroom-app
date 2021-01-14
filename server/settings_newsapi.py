1391969f3e52870d81782c28aeecf17ab61e1a52
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-14 13:09:13 +0100

    Settings for web and newsapi.

--- server/settings_newsapi.py
+++ server/settings_newsapi.py
@@ -1,1 +1,7 @@
-NEWS_API_ENABLED = True
+from superdesk.default_settings import env, strtobool
+from newsroom.web.default_settings import CORE_APPS, BLUEPRINTS
+
+
+# extend apps
+CORE_APPS.extend([])
+BLUEPRINTS.extend([])
