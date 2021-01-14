572dd8a94e140b27d2e56f1d09aa8e1fff4e4e2e
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-14 16:21:54 +0100

    Overloading templates.

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 from superdesk.default_settings import env, strtobool
 from newsroom.web.default_settings import CORE_APPS, BLUEPRINTS
 
+
+ABS_PATH = Path(__file__).resolve().parent
 
 # extend apps
 CORE_APPS.extend([])
