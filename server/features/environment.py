84b2a8aa9ef4c1be230e24eaec1aae4336823871
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-19 18:01:13 +0100

    Behave tests seed.

---
+++ server/features/environment.py
@@ -0,0 +1,36 @@
+from superdesk.tests.environment import setup_before_all, setup_before_scenario
+
+from newsroom.news_api.app import get_app as _get_app
+from settings_newsapi import CORE_APPS
+
+
+def get_app(*args, **kwargs):
+    # explicitly set testing to True
+    return _get_app(*args, testing=True, **kwargs)
+
+
+def before_all(context):
+    config = {
+        'CORE_APPS': CORE_APPS,
+        'INSTALLED_APPS': [],
+        'ELASTICSEARCH_FORCE_REFRESH': True,
+        'NEWS_API_ENABLED': True,
+        'NEWS_API_TIME_LIMIT_DAYS': 100
+    }
+    setup_before_all(context, config, app_factory=get_app)
+
+
+def before_scenario(context, scenario):
+    config = {
+        'CORE_APPS': CORE_APPS,
+        'INSTALLED_APPS': [],
+        'ELASTICSEARCH_FORCE_REFRESH': True,
+        'NEWS_API_ENABLED': True,
+        'NEWS_API_TIME_LIMIT_DAYS': 100
+    }
+
+    if 'rate_limit' in scenario.tags:
+        config['RATE_LIMIT_PERIOD'] = 300  # 5 minutes
+        config['RATE_LIMIT_REQUESTS'] = 2
+
+    setup_before_scenario(context, scenario, config, app_factory=get_app)
