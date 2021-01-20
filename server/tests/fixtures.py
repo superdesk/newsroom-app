538c9dad728482105d504c251fa55634da6cbbe0
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-20 17:29:04 +0100

    Pytest tests seed.

---
+++ server/tests/fixtures.py
@@ -0,0 +1,14 @@
+from newsroom.tests.fixtures import (  # noqa
+    PUBLIC_USER_ID,
+    PUBLIC_USER_FIRSTNAME,
+    PUBLIC_USER_LASTNAME,
+    PUBLIC_USER_NAME,
+    TEST_USER_ID,
+    items,
+    agenda_items,
+    init_items,
+    init_agenda_items,
+    init_auth,
+    setup_user_company,
+    init_company
+)
