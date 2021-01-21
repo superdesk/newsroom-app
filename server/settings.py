fb2b35abdcd649271da5e513c4b4092e8dd5569e
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-21 13:03:56 +0100

    Flake8

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from superdesk.default_settings import env, strtobool
+from superdesk.default_settings import env, strtobool # noqa
 from newsroom.web.default_settings import CORE_APPS, BLUEPRINTS
 
 
