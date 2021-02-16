79fea6dcf2261a4d4f134e9cd2885ef1f88f9a3c
pablopunk <pablo@pablopunk.com>
2021-02-16 17:43:51 +0100

    less code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,29 @@
 const path = require('path');
 const fs = require('fs');
-const newsroomWebpack = require('newsroom-core/webpack.config')
+const config = require('newsroom-core/webpack.config')
 
-for (const [name, path] of Object.entries(newsroomWebpack.entry)) {
+for (const [name, path] of Object.entries(config.entry)) {
     if (Array.isArray(path)) {
         // common modules
         if (fs.existsSync('node_modules/newsroom-core/node_modules')) {
             // when using linked repo
-            newsroomWebpack.entry[name] = path.map(f => `./node_modules/newsroom-core/node_modules/${f}`)
+            config.entry[name] = path.map(f => `./node_modules/newsroom-core/node_modules/${f}`)
         } else {
             // when using npm package
-            newsroomWebpack.entry[name] = path.map(f => `./node_modules/${f}`)
+            config.entry[name] = path.map(f => `./node_modules/${f}`)
         }
     } else {
-        newsroomWebpack.entry[name] = `./node_modules/newsroom-core/${path}`
+        config.entry[name] = `./node_modules/newsroom-core/${path}`
     }
 }
 
-module.exports = {
-    ...newsroomWebpack,
-    entry: {
-        ...newsroomWebpack.entry,
-        app: './app.js'
-    },
-    output: {
-        ...newsroomWebpack.output,
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        ...newsroomWebpack.resolve,
-        modules: [
-            ...newsroomWebpack.resolve.modules,
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, 'node_modules', 'newsroom-core', 'node_modules'),
-        ]
-    },
-    resolveLoader: {
-        ...newsroomWebpack.resolveLoader,
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, 'node_modules', 'newsroom-core', 'node_modules'),
-        ]
-    }
-};
+config.entry.app = './app.js'
+config.output.path = path.resolve(__dirname, 'dist')
+config.resolve.modules.push(
+    path.resolve(__dirname, 'node_modules'),
+    path.resolve(__dirname, 'node_modules', 'newsroom-core', 'node_modules')
+)
+config.resolveLoader.modules.push(
+    path.resolve(__dirname, 'node_modules'),
+    path.resolve(__dirname, 'node_modules', 'newsroom-core', 'node_modules'),
+)
