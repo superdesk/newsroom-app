a506a5a45f3d864d2e736dac8f616cd5eff0c0aa
pablopunk <pablo@pablopunk.com>
2021-02-16 16:44:52 +0100

    Reuse output from module

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const newsroomWebpack = require('newsroom-core/webpack.config')
 
+console.log(newsroomWebpack.module.rules[0])
+
 for (const [name, path] of Object.entries(newsroomWebpack.entry)) {
     if (Array.isArray(path)) {
         newsroomWebpack.entry[name] = path.map(f => `./node_modules/newsroom-core/node_modules/${f}`)
@@ -16,10 +18,8 @@ module.exports = {
         app: './app.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'http://localhost:8080/',
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[id].[chunkhash].js'
+        ...newsroomWebpack.output,
+        path: path.resolve(__dirname, 'dist')
     },
     resolve: {
         ...newsroomWebpack.resolve,
