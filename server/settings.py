8dd397fbab655928f786659a2c38bc932c15117a
Petr Jasek <jasekpetr@gmail.com>
2021-04-22 10:23:39 +0200

    clean up settings

--- server/settings.py
+++ server/settings.py
@@ -1,22 +1,12 @@
 import os
-from pathlib import Path
+import pathlib
 
-from superdesk.default_settings import env, strtobool # noqa
-from newsroom.web.default_settings import CORE_APPS, BLUEPRINTS
-
-
-ABS_PATH = Path(__file__).resolve().parent
+ABS_PATH = pathlib.Path(__file__).resolve().parent
 WEBPACK_MANIFEST_PATH = os.path.join(
-    os.path.dirname(__file__),
-    '..',
+    ABS_PATH.parent,
     'client',
     'dist',
     'manifest.json'
 )
 
-# extend apps
-CORE_APPS.extend([])
-BLUEPRINTS.extend([])
-
-# logging
-LOG_CONFIG_FILE = env('LOG_CONFIG_FILE', ABS_PATH / 'logging_config.yml')
+DEBUG = bool(os.environ.get('NEWSROOM_DEBUG'))
