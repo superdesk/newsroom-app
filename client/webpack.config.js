6119e04704ffb8e332650cb53e2ce8e38c2c1d4a
Petr Jasek <jasekpetr@gmail.com>
2021-10-08 14:22:44 +0200

    simplify path config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,1 @@
-const path = require('path');
-const config = require('newsroom-core/webpack.config')
-
-config.output.path = path.resolve(__dirname, 'dist')
-
-module.exports = config
+module.exports = require('newsroom-core/webpack.config')
--- server/settings.py
+++ server/settings.py
@@ -1,12 +1,4 @@
-import os
 import pathlib
 
-ABS_PATH = pathlib.Path(__file__).resolve().parent
-WEBPACK_MANIFEST_PATH = os.path.join(
-    ABS_PATH.parent,
-    'client',
-    'dist',
-    'manifest.json'
-)
-
-DEBUG = bool(os.environ.get('NEWSROOM_DEBUG'))
+SERVER_PATH = pathlib.Path(__file__).resolve().parent
+CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
