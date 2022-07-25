c01fa8b3dcd1f43386bbd6a736711b14c6f27a83
Petr Jasek <jasekpetr@gmail.com>
2022-07-25 13:22:55 +0200

    enable abstract field

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,13 @@
 import pathlib
 
+from newsroom.web.default_settings import CLIENT_CONFIG
+
+
 SERVER_PATH = pathlib.Path(__file__).resolve().parent
 CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
+
+DISPLAY_ABSTRACT = True
+
+CLIENT_CONFIG.update(
+    display_abstract=DISPLAY_ABSTRACT,
+)
