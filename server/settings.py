e5fe4fb242a26398fe3c55672e369ac1e522b9b2
Petr Jasek <jasekpetr@gmail.com>
2023-07-18 15:39:55 +0200

    fix lint

--- server/settings.py
+++ server/settings.py
@@ -4,4 +4,6 @@ import pathlib
 SERVER_PATH = pathlib.Path(__file__).resolve().parent
 CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
 
-WEBPACK_MANIFEST_PATH = os.environ.get("WEBPACK_MANIFEST_PATH", CLIENT_PATH.joinpath("dist"))
+WEBPACK_MANIFEST_PATH = os.environ.get(
+    "WEBPACK_MANIFEST_PATH", CLIENT_PATH.joinpath("dist")
+)
