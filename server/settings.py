747990bea35105f944fdb9c012959007cb8e3bdb
Petr Jasek <jasekpetr@gmail.com>
2023-11-06 13:34:52 +0100

    increase the limit for subject aggregations

--- server/settings.py
+++ server/settings.py
@@ -7,3 +7,11 @@ CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
 WEBPACK_MANIFEST_PATH = os.environ.get(
     "WEBPACK_MANIFEST_PATH", CLIENT_PATH.joinpath("dist", "manifest.json")
 )
+
+WIRE_AGGS = {
+    "genre": {"terms": {"field": "genre.name", "size": 50}},
+    "service": {"terms": {"field": "service.name", "size": 50}},
+    "subject": {"terms": {"field": "subject.name", "size": 100}},
+    "urgency": {"terms": {"field": "urgency"}},
+    "place": {"terms": {"field": "place.name", "size": 50}},
+}
