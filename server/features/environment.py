20d70fb153ffe9f6d8b70d25a3efe46d1bd5e642
Petr Jašek <jasekpetr@gmail.com>
2025-09-11 15:44:00 +0200

    update requirements (#258)
    * update requirements
    
    * add missing dependency
    
    * add missing dependency
    
    * fix failing test

--- server/features/environment.py
+++ server/features/environment.py
@@ -1,4 +1,4 @@
-from superdesk.tests.environment import setup_before_all, setup_before_scenario
+from superdesk.tests.environment import setup_before_all
 
 from newsroom.news_api.factory import get_app as _get_app
 from settings_newsapi import CORE_APPS
@@ -21,22 +21,3 @@ def before_all(context):
         "CACHE_TYPE": "null",
     }
     setup_before_all(context, config, app_factory=get_app)
-
-
-def before_scenario(context, scenario):
-    config = {
-        "BEHAVE": True,
-        "CORE_APPS": CORE_APPS,
-        "INSTALLED_APPS": [],
-        "ELASTICSEARCH_FORCE_REFRESH": True,
-        "NEWS_API_ENABLED": True,
-        "NEWS_API_TIME_LIMIT_DAYS": 100,
-        "NEWS_API_BEHAVE_TESTS": True,
-        "CACHE_TYPE": "null",
-    }
-
-    if "rate_limit" in scenario.tags:
-        config["RATE_LIMIT_PERIOD"] = 300  # 5 minutes
-        config["RATE_LIMIT_REQUESTS"] = 2
-
-    setup_before_scenario(context, scenario, config, app_factory=get_app)
